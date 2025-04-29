61a10cb215a4e8136302794ac2daec3b7d01ee83
NisanduGimhan <nisanduab@gmail.com>
2025-04-29 14:55:58 +0530

    implement header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
